9df34c6830ffc53b904294e74f1851a81d891bc1
eduuusama <esamayoam1@gmail.com>
2025-02-26 21:29:42 -0500

    Modified the style of both titles.

--- src/components/company/HeroSection.tsx
+++ src/components/company/HeroSection.tsx
@@ -2,18 +2,17 @@ import React from 'react';
 
 interface HeroSectionProps {
   title: string;
-  subtitle: string;
 }
 
-const HeroSection = ({ title, subtitle }: HeroSectionProps) => {
+const HeroSection = ({ title }: HeroSectionProps) => {
   return (
     <section className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
       <div className="text-center">
         <h1 className="text-4xl sm:text-5xl lg:text-[50px] font-medium text-center mb-4">
           {title}
         </h1>
-        <p className="mt-6 max-w-2xl font-normal mx-auto text-xl text-gray-600">
-          {subtitle}
+        <p className="text-xl sm:text-2xl lg:text-[32px] text-gray-500 font-normal mb-8 mt-6 max-w-3xl mx-auto">
+          Empower e-commerce entrepreneurs with advanced AI agents that deliver insights, drive decisions, and do the heavy day-to-day lifting.
         </p>
       </div>
     </section>
