d11c2eddc24dbec9bdae2284e3e0bee589e12ed5
eduuusama <esamayoam1@gmail.com>
2025-02-26 21:36:31 -0500

    Modified formatting for header and subheader.

--- src/components/company/HeroSection.tsx
+++ src/components/company/HeroSection.tsx
@@ -9,9 +9,10 @@ const HeroSection = ({ title }: HeroSectionProps) => {
     <section className="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
       <div className="text-center">
         <h1 className="text-4xl sm:text-5xl lg:text-[50px] font-medium text-center mb-4">
-          {title}
+          Freeing up business owners valuable<br className="hidden lg:inline" /> 
+          time to focus on their passion.
         </h1>
-        <p className="text-xl sm:text-2xl lg:text-[32px] text-gray-500 font-normal mb-8 mt-6 max-w-3xl mx-auto">
+        <p className="text-xl sm:text-2xl lg:text-[28.8px] text-gray-500 font-normal mb-8 mt-6 max-w-[90%] lg:max-w-[95%] mx-auto">
           Empower e-commerce entrepreneurs with advanced AI agents that deliver insights, drive decisions, and do the heavy day-to-day lifting.
         </p>
       </div>
