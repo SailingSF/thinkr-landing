b2d92c598ffb7776be97064a638b2611a8b115a0
Max <maxabouchar@gmail.com>
2025-04-04 14:08:39 -0500

    add meta pixel directly

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,9 @@ const inter = Inter({
   fallback: ['system-ui', 'sans-serif'], // Provide fallback fonts
 })
 
-// Get GTM ID from environment variable
+// Get tracking IDs from environment variable
 const gtmId = process.env.NEXT_PUBLIC_GTM_ID || 'GTM-KSLVDRZF'
+const metaPixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID || '4182300618666599'
 
 export const metadata: Metadata = {
   title: "thinkr - Automate Everyday Shopify Operations",
@@ -87,16 +88,36 @@ export default function RootLayout({
           <iframe src="https://www.googletagmanager.com/ns.html?id=${gtmId}"
           height="0" width="0" style="display:none;visibility:hidden"></iframe>
         `}} />
+        {/* Meta Pixel - Body Section (noscript) */}
+        <noscript dangerouslySetInnerHTML={{ __html: `
+          <img height="1" width="1" style="display:none"
+          src="https://www.facebook.com/tr?id=${metaPixelId}&ev=PageView&noscript=1" />
+        `}} />
         {children}
         <Analytics />
         <SpeedInsights />
-        {/* Google Tag Manager - Script Section moved here for better performance */}
+        {/* Google Tag Manager Script */}
         <Script id="google-tag-manager" strategy="afterInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','${gtmId}');`}
+        </Script>
+        {/* Meta Pixel Script */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '${metaPixelId}');
+          fbq('track', 'PageView');
+        `}
         </Script>
       </body>
     </html>
